53510d78ced1c6398ddfcc443982c06c9de83a8d
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-25 13:01:17 -0500

    We create the class SnapshotHolder

--- app/src/main/java/com/example/snapshots/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/snapshots/Fragments/HomeFragment.kt
@@ -5,18 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.example.snapshots.R
+import com.example.snapshots.Snapshot
+import com.example.snapshots.databinding.ItemSnapshotBinding
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+
+
+
+/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+      ----------- INNER CLASS ------------
+ -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
+
+inner class SnapshotHolder(view: View):RecyclerView.ViewHolder(view){
+    val binding=ItemSnapshotBinding.bind(view)
+
+    fun setListener(snapshot: Snapshot){
+
+    }
+}
+
+
+
 
 
 }
