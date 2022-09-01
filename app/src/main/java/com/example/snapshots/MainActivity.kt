03239c10985403a246e4cacec219035f5d27db2b
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-09-01 07:58:36 -0500

    We create the design to close session and onActivityResult deprecado

--- app/src/main/java/com/example/snapshots/MainActivity.kt
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -1,24 +1,53 @@
 package com.example.snapshots
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.snapshots.Fragments.AddFragment
 import com.example.snapshots.Fragments.HomeFragment
 import com.example.snapshots.Fragments.ProfileFragment
 import com.example.snapshots.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val RC_SING_IN =21
 
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mActivityFragment:Fragment
     private  lateinit var  mFragmentManager: FragmentManager
+
+    private lateinit var mAuthiListener:FirebaseAuth.AuthStateListener
+    private var mFirebaseAuth:FirebaseAuth?=null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        setupAuth()
+
         initBottomNav()
+    }
+
+    private fun setupAuth() {
+        mFirebaseAuth= FirebaseAuth.getInstance()
+        mAuthiListener=FirebaseAuth.AuthStateListener {
+            val user =it.currentUser
+            if(user==null){
+                val authUI= AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(Arrays.asList(AuthUI.IdpConfig.EmailBuilder().build(),
+                        AuthUI.IdpConfig.GoogleBuilder().build())).build()
+                    startActivityForResult(authUI,RC_SING_IN)
+            }
+        }
     }
 
     private fun initBottomNav() {
@@ -49,5 +78,29 @@ class MainActivity : AppCompatActivity() {
         mFragmentManager.beginTransaction().hide(mActivityFragment).show(nameFragment).commit()
         mActivityFragment=nameFragment
         return  true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth?.addAuthStateListener(mAuthiListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth?.removeAuthStateListener(mAuthiListener)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==RC_SING_IN){
+            if(resultCode== RESULT_OK){
+                Toast.makeText(this,"Welcome ....",Toast.LENGTH_SHORT).show()
+            }else{
+                if(IdpResponse.fromResultIntent(data)==null){ // Botton Back press
+                    finish()
+                }
+            }
+        }
     }
 }
