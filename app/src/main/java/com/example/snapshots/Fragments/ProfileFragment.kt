5ebd3ae1334b2ddb377d1b7abbf6e8f865781c6f
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-09-06 08:03:56 -0500

    We implement the refresh fuction of the interface FragmentAux

--- app/src/main/java/com/example/snapshots/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/snapshots/Fragments/ProfileFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.example.snapshots.FragmentAux
 import com.example.snapshots.R
+import com.example.snapshots.SnapshotsApplication
 import com.example.snapshots.databinding.FragmentProfileBinding
 import com.firebase.ui.auth.AuthUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(),FragmentAux {
 private lateinit var mBinding:FragmentProfileBinding
 
 
@@ -23,19 +27,47 @@ private lateinit var mBinding:FragmentProfileBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(mBinding){
-            tvName.text=FirebaseAuth.getInstance().currentUser?.displayName?:" Nama user"
-            tvGmail.text=FirebaseAuth.getInstance().currentUser?.email?:" @gmail.com"
-            btnSingOut.setOnClickListener { sigOut() }
+        refresh()
+        setupButton()
 
+
+    }
+
+    private fun setupButton() {
+        mBinding.btnSingOut.setOnClickListener {
+            context?.let {
+                MaterialAlertDialogBuilder(it)
+                    .setTitle("¿Desea cerrar la sesion actual?")
+                    .setPositiveButton("Close") { _, _ ->
+                        singOut()
+                    }
+
+                    .setNegativeButton("Cancelar", null)
+                    .show()
+            }
         }
     }
 
-    private fun sigOut() {
-       context?.let {conte->
-           AuthUI.getInstance().signOut(conte)
-               .addOnCompleteListener { Toast.makeText(context," See  you later..", Toast.LENGTH_SHORT).show() }
-       }
+    private fun singOut() {
+        context?.let {
+            AuthUI.getInstance().signOut(it)
+                .addOnCompleteListener {
+                    Toast.makeText(context, "You see later", Toast.LENGTH_SHORT).show()
+                    mBinding.tvName.text = ""
+                    mBinding.tvGmail.text = ""
+
+                    (activity?.findViewById(R.id.bottomNav) as? BottomNavigationView)?.selectedItemId =
+                        R.id.action_home
+                }
+        }
+    }
+
+
+    override fun refresh() {
+        with(mBinding) {
+            tvName.text=FirebaseAuth.getInstance().currentUser?.displayName?:" Nama user"
+            tvGmail.text=FirebaseAuth.getInstance().currentUser?.email?:" @gmail.com"
+        }
     }
 
 
