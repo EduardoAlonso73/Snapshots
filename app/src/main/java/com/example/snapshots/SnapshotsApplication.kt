441f758a3a6c7db42fd7de5b54497515528e7a74
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-09-06 08:11:32 -0500

    We create the class SnapshotsAplication to separate ths constants used in  other classes

--- app/src/main/java/com/example/snapshots/SnapshotsApplication.kt
+++ app/src/main/java/com/example/snapshots/SnapshotsApplication.kt
@@ -1,4 +1,14 @@
 package com.example.snapshots
 
-class SnapshotsApplication {
+import android.app.Application
+import com.google.firebase.auth.FirebaseUser
+
+
+class SnapshotsApplication : Application() {
+    companion object {
+        const val PATH_SNAPSHOTS = "snapshots"
+        const val PROPERTY_LIKE_LIST = "likeList"
+
+        lateinit var currentUser: FirebaseUser
+    }
 }
