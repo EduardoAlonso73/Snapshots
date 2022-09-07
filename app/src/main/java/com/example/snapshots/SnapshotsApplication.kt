4e0594b0e210583cb5137e5e12f90b4d408769c1
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-09-06 21:05:44 -0500

    We add the const CODE_VALUE

--- app/src/main/java/com/example/snapshots/SnapshotsApplication.kt
+++ app/src/main/java/com/example/snapshots/SnapshotsApplication.kt
@@ -8,6 +8,7 @@ class SnapshotsApplication : Application() {
     companion object {
         const val PATH_SNAPSHOTS = "snapshots"
         const val PROPERTY_LIKE_LIST = "likeList"
+        const val  CODE_VALUE_EXP=-13010
 
         lateinit var currentUser: FirebaseUser
     }
